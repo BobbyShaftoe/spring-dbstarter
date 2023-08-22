abf129c7c38134a737ee5ceae801b84b9876ca26
BobbyShaftoe <nick731@gmail.com>
2023-08-22 23:39:22 +0800

    Create repository, save a user

--- src/main/java/com/nick/db/starter/dbstarter/DbStarterApplication.java
+++ src/main/java/com/nick/db/starter/dbstarter/DbStarterApplication.java
@@ -1,9 +1,11 @@
 package com.nick.db.starter.dbstarter;
 
-import com.nick.db.starter.dbstarter.repository.Users;
+import com.nick.db.starter.dbstarter.model.Users;
+import com.nick.db.starter.dbstarter.repository.UsersRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DbStarterApplication {
@@ -12,6 +14,17 @@ public class DbStarterApplication {
 		SpringApplication.run(DbStarterApplication.class, args);
 	}
 
-//	CommandLineRunner commandLineRunner(Users users)
+	@Bean
+	CommandLineRunner commandLineRunner(UsersRepository usersRepository) {
+		return args -> {
+			Users bobby = new Users(
+					"Bobby",
+					"Shaftoe",
+					"bobby.shaftoe@gmail.com",
+					40
+			);
+			usersRepository.save(bobby);
+		};
+	}
 
 }
